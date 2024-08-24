b977a1e9b41082b2f20651c3a71b865266b6f6e8
Ashik-Himel <ashikujjamanhimel@gmail.com>
2024-08-24 23:33:15 +0600

    moved login, register controllers in the user folder

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const login = require('../controllers/login');
-const register = require('../controllers/register');
+const login = require('../controllers/user/login');
+const register = require('../controllers/user/register');
 
 const router = express.Router();
 
